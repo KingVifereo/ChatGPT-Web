288d745b3eebe53c8258d925f6daeead9f6f1634
KingVifereo <106603987+KingVifereo@users.noreply.github.com>
2023-04-18 22:52:45 +0800

    update bbbbb

--- src/components/common/Setting/General.vue
+++ src/components/common/Setting/General.vue
@@ -20,11 +20,11 @@ const theme = computed(() => appStore.theme)
 
 const userInfo = computed(() => userStore.userInfo)
 
-const avatar = ref(userInfo.value.avatar ?? '')
+//const avatar = ref(userInfo.value.avatar ?? '')
 
 const name = ref(userInfo.value.name ?? '')
 
-const description = ref(userInfo.value.description ?? '')
+//const description = ref(userInfo.value.description ?? '')
 
 const language = computed({
   get() {
@@ -123,6 +123,7 @@ function handleImportButtonClick(): void {
 
 <template>
   <div class="p-4 space-y-5 min-h-[200px]">
+	/*
 	  <div class="flex items-center space-x-4">
         <span class="flex-shrink-0 w-[100px]">{{ $t('setting.avatarLink') }}</span>
        <div class="flex-1">
@@ -132,6 +133,7 @@ function handleImportButtonClick(): void {
          {{ $t('common.save') }}
        </NButton>
     </div>
+		*/
     <div class="space-y-6">
       <div class="flex items-center space-x-4">
         <span class="flex-shrink-0 w-[100px]">{{ $t('setting.name') }}</span>
@@ -142,6 +144,7 @@ function handleImportButtonClick(): void {
           {{ $t('common.save') }}
         </NButton>
      </div>
+		 /*
 		 <div class="flex items-center space-x-4">
         <span class="flex-shrink-0 w-[100px]">{{ $t('setting.description') }}</span>
         <div class="flex-1">
@@ -151,6 +154,7 @@ function handleImportButtonClick(): void {
           {{ $t('common.save') }}
         </NButton>
       </div>
+			*/
       <div
         class="flex items-center space-x-4"
         :class="isMobile && 'items-start'"
