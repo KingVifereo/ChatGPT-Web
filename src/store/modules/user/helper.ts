dbd54f319b513b352e7f963b9b84884a4b790100
KingVifereo <106603987+KingVifereo@users.noreply.github.com>
2023-04-18 21:41:25 +0800

    update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://github.com/KingVifereo/ChatGPT-Web/main/src/assets/avatar.jpg',
+      name: 'SunShijiang',
+      description: 'Star on <a href="https://github.com/KingVifereo/ChatGPT-Web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
